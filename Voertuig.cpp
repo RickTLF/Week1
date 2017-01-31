2ebdfe1bc0e10f8736616100dab47cc1d124cab6
Rick Kock <rick.92.tlf@gmail.com>
2017-01-31 06:06:14 +0100

    Complete project: use substr to get the string between two indexes.

--- Voertuig.cpp
+++ Voertuig.cpp
@@ -5,10 +5,46 @@
 Voertuig::Voertuig(string merk, string soort, int jaar) {
 	this->merk = merk;
 	this->soort = soort;
-	this->jaartal = std::to_string(jaar);
+	this->jaartal = to_string(jaar);
 	this->jaar = jaar;
 }
 
 void Voertuig::geefWeer() { //zet data op het scherm
-	cout << soort << " merk: " << merk << " gekocht: " << jaar << endl;
+	cout << "Soort: " << soort << " merk: " << merk << " gekocht: " << jaar << endl;
+}
+
+void Voertuig::naarJaar() {
+	char hetJaar[5];
+	strncpy_s(hetJaar, jaartal.c_str(), 4);
+	jaar = zetJaarOm(hetJaar);
+}
+
+int zetJaarOm(char jaar[6]) {
+	int aantalJaar = 0;
+	for (int i = 0; i<4; i++) {
+		aantalJaar *= 10;
+		aantalJaar += jaar[i] - 48;
+	}
+	if (aantalJaar > 1900 && aantalJaar < 2100) return aantalJaar;
+	return 0;
+}
+
+//"1990 motor bmw" ,"2000 auto Opel", "1984 vrachtwagen Daf"
+void Voertuig::leesData(char data[]) {
+	jaartal.clear();
+	jaartal = string(data).substr(0, string(data).find_first_of(" "));
+	naarJaar();
+
+	/*
+	 * Use 'find_first_of(" ") + 1' and 'find_last_of(" ")' 
+	 * to get the two indexes of any string between spaces.
+	 * use '+ 1' to skip the first space.
+	 */
+	string tempString = string(data).substr(string(data).find_first_of(" ") + 1, string(data).find_last_of(" "));
+	
+	soort.clear();
+	soort = tempString.substr(0, tempString.find_first_of(" "));
+	
+	merk.clear();
+	merk = tempString.substr(tempString.find_first_of(" ") + 1, tempString.find_last_of(" "));
 }
--- cppWeek1.cpp
+++ cppWeek1.cpp
@@ -2,7 +2,7 @@
 //
 
 #include "stdafx.h"
-#include "MyTimes.h"
+// #include "MyTimes.h"
 #include "Voertuig.h"
 
 /*int main(void) {
@@ -14,19 +14,21 @@
 
 int main(int argc, char* argv[])
 {
-	char voertuigen[][80] = { "1990 motor bmw" ,
+	char voertuigen[][80] = { 
+		"1990 motor bmw" ,
 		"2000 auto Opel",
-		"1984 vrachtwagen Daf" };
+		"1984 vrachtwagen Daf"
+	};
 
 	Voertuig voertuig("Yahama", "boot", 2000);
 	Voertuig vt;
 	voertuig.geefWeer();
-	// vt.leesData(voertuigen[0]);
-	// vt.geefWeer();
-	// vt.leesData(voertuigen[1]);
-	// vt.geefWeer();
-	// vt.leesData(voertuigen[2]);
-	// vt.geefWeer();
+	vt.leesData(voertuigen[0]);
+	vt.geefWeer();
+	vt.leesData(voertuigen[1]);
+	vt.geefWeer();
+	vt.leesData(voertuigen[2]);
+	vt.geefWeer();
 	cin.get();
 	return 0;
 }
